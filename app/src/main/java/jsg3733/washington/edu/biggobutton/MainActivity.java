7aa4a377acf6cc2c4d244881e6976a132de07f31
Jacob Gilmore <jsg3733@uw.edu>
2015-01-20 14:42:38 -0800

    Committed the button that counts clicks

---
+++ app/src/main/java/jsg3733/washington/edu/biggobutton/MainActivity.java
@@ -0,0 +1,53 @@
+package jsg3733.washington.edu.biggobutton;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        count = 0;
+        final Button push = (Button) findViewById(R.id.btnBigButton);
+        push.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v) {
+                        count++;
+                         push.setText("You have pushed me " + count + " times!");
+            }
+            });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
